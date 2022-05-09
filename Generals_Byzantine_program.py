94bf9e24bfd62d7ec3922ba0f2d85fc96e454c7c
TofigBakhshiyev <tofiqbakhshiyev@gmail.com>
2022-05-09 19:15:14 +0300

    Tofig Bakhshiyev

---
+++ Generals_Byzantine_program.py
@@ -0,0 +1,136 @@
+import threading
+import datetime
+date_time = datetime.datetime.now()
+import rpyc
+import sys
+
+# global variables
+generals = {}
+listofPorts = {}
+primary_general_id = 0
+
+class Service(rpyc.Service):
+    def exposed_order(self, id, order):
+        generals[id].majority = order
+
+    def exposed_verfy_the_order(self, id):
+        if generals[id].state == "F":
+            return False, generals[id].majority
+        else:
+            return True, generals[id].majority
+
+def sendtheorder(order):
+    if len(generals) <= 3:
+        failed_generals = [gn for gn in generals.values() if gn.state == "F"]
+        generals[primary_general_id].majority = order
+        for g in generals.values():
+            print(f"G{g.id}, {g.type}, majority={g.majority}, {g.state}")
+        print(f"Execute order: cannot be determined – not enough generals in the system! {len(failed_generals)} faulty node in the system - {len(generals)-1} out of {len(generals)} quorum not consistent")
+    else:
+        generals[primary_general_id].majority = order
+        generals[primary_general_id].broadcastOrder(order)
+        # verify the order
+        all_answers_from_all_generals_between_messaging = []
+        ids = list(generals.keys())
+        failed_general_count = 0
+        for gid in range(1, len(ids)):
+            answers, failed_general = generals[ids[gid]].verifyOrder()
+            all_answers_from_all_generals_between_messaging.append(answers)
+            failed_general_count = failed_general
+
+        shouldDoOrder = all(general_answer == all_answers_from_all_generals_between_messaging[0] for general_answer in all_answers_from_all_generals_between_messaging)
+
+        if shouldDoOrder == True:
+            for g in generals.values():
+                print(f"G{g.id}, {g.type}, majority={g.majority}, {g.state}")
+            if failed_general_count == 0:
+                print(f"Execute order: {order}! Non-faulty nodes in the system – {len(all_answers_from_all_generals_between_messaging)} out of {len(generals)} quorum suggest {order}")
+            else:
+                print(f"Execute order: {order}! {failed_general_count} faulty nodes in the system – {len(all_answers_from_all_generals_between_messaging) - failed_general_count} out of {len(generals)} quorum suggest {order}")
+    generals[primary_general_id].return_undefined_state("undefined")
+
+def deleteGeneral(id):
+    if id in generals and generals[id].type != "primary":
+        del generals[id]
+        del listofPorts[id]
+    elif generals[id].type == "primary":
+        # needs an election for primary general
+        del generals[primary_general_id]
+        del listofPorts[primary_general_id]
+        election(primary_general_id + 1)
+    else:
+        print("The general is not exist with this id, please try again!")
+    printStates()
+
+def election(id):
+    global primary_general_id
+    generals[id].type = "primary"
+    primary_general_id = id
+
+def add_k_number_of_generals(K):
+    last_id = list(generals.keys())[-1] + 1
+    last_port = list(listofPorts.keys())[-1] + 1
+    for gid in range(K):
+        server = rpyc.utils.server.ThreadedServer(Service, port = last_port)
+        general = General(last_id, "secondary", "undefined", "NF", server)
+        generals[last_id] = general
+        generals[last_id].daemon = True
+        generals[last_id].start()
+        listofPorts[last_id] = last_port
+        last_id += 1
+        last_port += 1
+
+def createGenerals(N):
+    port = 2000
+    global primary_general_id
+    for gid in range(1, N + 1):
+        if gid == 1:
+            primary_general_id = gid
+            server = rpyc.utils.server.ThreadedServer(Service, port = port)
+            general = General(gid, "primary", "undefined", "NF", server)
+        else:
+            server = rpyc.utils.server.ThreadedServer(Service, port = port)
+            general = General(gid, "secondary", "undefined", "NF", server)
+        generals[gid] = general
+        generals[gid].daemon = True
+        generals[gid].start()
+        listofPorts[gid] = port
+        port += 1
+
+def printStates():
+    for g in generals.values():
+        print(f"G{g.id}, {g.type}, {g.state}")
+
+def changeType(id, state):
+    if state == "faulty":
+        generals[id].state = "F"
+
+def main(argument):
+    N = int(argument[1])
+    if N < 0:
+        print("Try to run program again, N should not be below the zero")
+    else:
+        createGenerals(N)
+        while True:
+            command = input("Enter on of the commands (actual-order, g-state id faulty, g-state, g-kill id, g-add K):").lower().split(" ")
+            cmd = command[0]
+
+            if cmd == "actual-order":
+                order = str(command[1])
+                sendtheorder(order)
+            elif cmd == "g-state":
+                if len(command) > 1:
+                    id = int(command[1])
+                    state = command[2]
+                    changeType(id, state)
+                else:
+                    printStates()
+            elif cmd == "g-kill":
+                id = int(command[1])
+                deleteGeneral(id)
+            elif cmd == "g-add":
+                K = int(command[1])
+                add_k_number_of_generals(K)
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ README.md
@@ -0,0 +1,24 @@
+### Distributed Systems Mini Project 2
+Description: Basicly this project is all about implementing The Byzantine General’s problem with Consensus based on lecture notes [project description](https://courses.cs.ut.ee/2022/ds/spring/uploads/Main/Mini-project2-DS2022.pdf)
+
+Tofiq Bakhshiyev
+Anton Rudchenko
+
+#### Requirments
+- Python 3 version
+- OS is Windows 10
+
+#### Instalation
+- pip install -r requirements.txt
+
+#### Run the project
+- python .\Generals_Byzantine_program.py 4
+- List of commands (actual-order, g-state id faulty, g-state, g-kill id, g-add K):
+ - actual-order attack or retreat
+ - g-state id faulty
+ - g-state
+ - g-kill id
+ - g-add K
+
+#### Example video
+- [Project video]("url")
